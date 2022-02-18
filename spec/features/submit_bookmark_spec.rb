c780dedbec160af0e533ba9b4348caf8fbe43aa0
Jaroslaw Roman Cudzich <internet.jarek@gmail.com>
2022-02-18 18:47:25 +0000

    Added a title column to the bookmarks table and displayed it on the homepage in place of the URL link.

--- spec/features/submit_bookmark_spec.rb
+++ spec/features/submit_bookmark_spec.rb
@@ -7,9 +7,10 @@ require 'pg'
 feature 'submitting bookmark' do
   scenario 'submit bookmark through the browser' do
     visit('/bookmarks/new')
-    fill_in('url', with: 'http://www.yahoo.com')
+    fill_in('url', with: 'http://www.example.org')
+    fill_in('title', with: 'Test Bookmark')
     click_button('Submit')
 
-    expect(page).to have_content 'http://www.yahoo.com'
+    expect(page).to have_link('Test Bookmark', href: 'http://www.example.org')
   end
 end
